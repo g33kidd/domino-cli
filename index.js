ef59de9a3275c7c36e75fd65927b16ae21118894
Joshua Kidd <kidd.josh.343@gmail.com>
2015-06-08 13:57:07 -0600

    Added wp-cli integration for communicating with WordPress

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var fs = require('fs');
 var colors = require('colors');
 var cli = require('./lib/cli');
 var util = require('./lib/utils');
-var config = JSON.parse(fs.readFileSync("domino.json"));
+var wordpress = require('./lib/wordpress');
 
-var dominoHeader = "Working on: " + config.theme.name + "\nPowered by: domino-cli\n=======================";
-console.log(dominoHeader.cyan);
+if(fs.existsSync("domino.json")) {
+  var config = JSON.parse(fs.readFileSync("domino.json"));
+  var dominoHeader = "Working on: " + 
+      config.theme.name +  "\nPowered by: domino-cli\nWorking Directory: " +
+      process.cwd() + "\n=======================\n";
+  console.log(dominoHeader.cyan);
+}else{
+  console.log("Use 'domino init' to initialize a new domino project.".magenta);
+}
+
 cli.parse(process.argv);
