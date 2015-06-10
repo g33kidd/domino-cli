a53991cbbdea9085ec41b64809a42e7edfd00da2
Joshua Kidd <kidd.josh.343@gmail.com>
2015-06-10 01:48:42 -0600

    Added sync for WordPress site deployments

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ var util = require('./lib/utils');
 var wordpress = require('./lib/wordpress');
 
 if(fs.existsSync("domino.json")) {
-  var config = JSON.parse(fs.readFileSync("domino.json"));
-  var dominoHeader = "Working on: " + 
-      config.theme.name +  "\nPowered by: domino-cli\nWorking Directory: " +
-      process.cwd() + "\n=======================\n";
-  console.log(dominoHeader.cyan);
-}else{
-  console.log("Use 'domino init' to initialize a new domino project.".magenta);
+  config = JSON.parse(fs.readFileSync("domino.json"));
+  util.showHeader();
 }
 
 cli.parse(process.argv);
